05514897a552049965feda1883522e69127fda28
geekinryxnn <ryankiratu67@gmail.com>
2025-05-13 08:02:47 +0300

    new code

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,8 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    
+    # No need to redefine fill_up_tank() here unless we want different behavior
+    # It will automatically inherit from Vehicle
